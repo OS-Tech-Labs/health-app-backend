0fc902744976faadb4c2b8a761e08c8ef9af0e98
samithkavishke <samithkarunathilake@gmail.com>
2023-03-13 20:29:40 +0530

    done somewhat

---
+++ src/api/index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import helmet from 'helmet'
+import morgan from 'morgan'
+
+import LoginRoutes from './routes/Login'
+
+dotenv.config()
+const app = express()
+
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
+app.use(morgan('common'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extendded: false }))
+app.use(cors())
+
+/* ROUTES */
+app.use('/login', LoginRoutes)
