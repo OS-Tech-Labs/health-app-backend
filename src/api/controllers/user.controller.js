7a6e0f9e6ba32f9c42fff833942770f8c4092764
samithkavishke <55777141+samithkavishke@users.noreply.github.com>
2023-09-02 15:01:30 +0530

    pg db working

--- src/api/controllers/user.controller.js
+++ src/api/controllers/user.controller.js
@@ -1,28 +1,51 @@
-import { getUserHash, comparePassword } from '../services/user.service.js'
+import {
+  comparePassword,
+  generateHash,
+} from "../services/user.service.js"
+import  User  from "../models/user.model.js"
 
 const login = async (req, res) => {
   const username = req.body.username
   const password = req.body.password
 
-  const hash = await getUserHash(username)
-  console.log(password, hash)
-  if (!hash) {
-    return res.send({ approved: false })
-  }
+  try {
+    const user = await User.getUserByUsername(username)
 
-  const match = await comparePassword(password, hash)
-  console.log(username, password, hash)
-  if (match) {
-    console.log('Login successful')
-    return res.send({ approved: true })
-  } else {
-    console.log('Login failed')
+    if (user === null) {
+      console.log("User not found")
+      return res.send({ approved: false })
+    }
+    const hash = user.hash
+    const match = await comparePassword(password, hash)
+    console.log(username, password, hash)
+    if (match) {
+      console.log("Login successful")
+      return res.send({ approved: true })
+    } else {
+      console.log("Login failed")
+      return res.send({ approved: false })
+    }
+  } catch (err) {
+    console.log(err)
     return res.send({ approved: false })
   }
 }
 
 const register = async (req, res) => {
-  console.log('Registration function')
+  const username = req.body.username
+  const password = req.body.password
+  const email = req.body.email
+  const type = req.body.type
+
+  const hash = await generateHash(password)
+  try {
+    const user = await User.createUser(username, hash, type, email)
+  } catch (err) {
+    console.log(err)
+    return res.send({ approved: false })
+  }
+
+  console.log("Registration function")
 }
 
 export default { login, register }
--- src/api/services/user.service.js
+++ src/api/services/user.service.js
@@ -1,22 +1,9 @@
-import { PrismaClient } from '@prisma/client'
-import bcrypt from 'bcrypt'
-
-const prisma = new PrismaClient()
-
-export const getUserHash = async (username) => {
-  const user = await prisma.user.findFirst({
-    where: {
-      OR: [
-        { email_address: username },
-        { username }
-      ]
-    },
-    select: { hash: true }
-  })
-
-  return user ? user.hash : null
-}
+import bcrypt from "bcrypt"
 
 export const comparePassword = async (password, hash) => {
   return await bcrypt.compare(password, hash)
+}
+
+export const generateHash = async (password) => {
+  return await bcrypt.hash(password, 10)
 }
